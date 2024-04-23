bb806837b220764d91ce2815a5eeb41aae1229d1
Rodrigo <113945542+youmelo1@users.noreply.github.com>
2024-04-22 21:54:03 -0300

    Update Cinema.java

--- Laboratorio2_Cinema/src/Cinema.java
+++ Laboratorio2_Cinema/src/Cinema.java
@@ -2,7 +2,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
-    private int[][] assentos = new int[12][14]; 
+    private int[][] assentos = new int[12][14];
+
+    Scanner scanner;
+
+    public Cinema() {
+        this.scanner = new Scanner(System.in);
+    }
 
     public void lugaresInfo() {
         char fileira = 'A';
@@ -20,24 +26,24 @@ public class Cinema {
     public void reservar(int fileira, int lugar) {
         if (fileira >= 0 && fileira < 12 && lugar >= 1 && lugar <= 14) {
             if (assentos[fileira][lugar - 1] == 1) {
-                System.out.println("Lugar já ocupado!");
+                System.out.println("Lugar j� ocupado!");
             } else {
                 assentos[fileira][lugar - 1] = 1;
             }
         } else {
-            System.out.println("Este assento não existe!");
+            System.out.println("Este assento n�o existe!");
         }
     }
 
     public void cancelar(int fileira, int lugar) {
         if (fileira >= 0 && fileira < 12 && lugar >= 1 && lugar <= 14) {
             if (assentos[fileira][lugar - 1] == 0) {
-                System.out.println("Lugar não está ocupado!");
+                System.out.println("Lugar n�o est� ocupado!");
             } else {
                 assentos[fileira][lugar - 1] = 0;
             }
         } else {
-            System.out.println("Este assento não existe!");
+            System.out.println("Este assento n�o existe!");
         }
     }
 
@@ -58,79 +64,80 @@ public class Cinema {
     }
 
     public void reservarAssento() {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Gostaria de reservar o assento de qual fileira? (A-L): ");
-        String input = scanner.next().toUpperCase(); 
+        String input = scanner.next().toUpperCase();
         char fileira = input.charAt(0);
         System.out.println("Qual assento deseja reservar? (1-14): ");
         int lugar = scanner.nextInt();
         int fileiraIndex = fileira - 'A';
         reservar(fileiraIndex, lugar);
+
     }
 
     public void cancelarReserva() {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Gostaria de cancelar o assento de qual fileira? (A-L): ");
-        String input = scanner.next().toUpperCase(); // Converte a entrada para maiúsculas
+        String input = scanner.next().toUpperCase(); // Converte a entrada para mai�sculas
         char fileira = input.charAt(0);
         System.out.println("Qual reserva gostaria de cancelar? (1-14): ");
         int lugar = scanner.nextInt();
         int fileiraIndex = fileira - 'A';
         cancelar(fileiraIndex, lugar);
+
     }
 
+    public void menu() {
 
-	public void menu () {
-		Scanner texto = new Scanner(System.in);
-		System.out.println("Bem-vindo ao cinema, o que gostaria de fazer?");
-		System.out.println("\n1)Mostrar mapa dos assentos(os lugares ocupados sï¿½o marcados com o nï¿½mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
-		System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
-		int esc = texto.nextInt();
+        System.out.println("Bem-vindo ao cinema, o que gostaria de fazer?");
+        System.out.println(
+                "\n1)Mostrar mapa dos assentos(os lugares ocupados s�o marcados com o n�mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
+        System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
+        int esc = scanner.nextInt();
 
-		while(esc != 5) {
-			switch(esc) {
-			case 1:
-				lugaresInfo();
-				System.out.println("\n1)Mostrar mapa dos assentos(os lugares ocupados sï¿½o marcados com o nï¿½mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
-				System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
-				esc = texto.nextInt();
-				break;
-			case 2:
-				assentosLivresOcupados();
-				System.out.println("\n1)Mostrar mapa dos assentos(os lugares ocupados sï¿½o marcados com o nï¿½mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
-				System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
-				esc = texto.nextInt();
-				break;
-			case 3:
-				reservarAssento();
-				System.out.println("\n1)Mostrar mapa dos assentos(os lugares ocupados sï¿½o marcados com o nï¿½mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
-				System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
-				esc = texto.nextInt();
-				break;
-			case 4:
-				cancelarReserva();
-				System.out.println("\n1)Mostrar mapa dos assentos(os lugares ocupados sï¿½o marcados com o nï¿½mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
-				System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
-				esc = texto.nextInt();
-				break;
-			case 5:
-				System.out.println("Obrigado!");
-				break;
-			default:
-				System.out.println("Essa nï¿½o ï¿½ uma opï¿½ï¿½o disponï¿½vel");
-				System.out.println("\n1)Mostrar mapa dos assentos(os lugares ocupados sï¿½o marcados com o nï¿½mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
-				System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
-				esc = texto.nextInt();
-				break;
-			}
-		}
-	}
+        while (esc != 5) {
+            switch (esc) {
+                case 1:
+                    lugaresInfo();
+                    System.out.println(
+                            "\n1)Mostrar mapa dos assentos(os lugares ocupados s�o marcados com o n�mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
+                    System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
+                    esc = scanner.nextInt();
+                    break;
+                case 2:
+                    assentosLivresOcupados();
+                    System.out.println(
+                            "\n1)Mostrar mapa dos assentos(os lugares ocupados s�o marcados com o n�mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
+                    System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
+                    esc = scanner.nextInt();
+                    break;
+                case 3:
+                    reservarAssento();
+                    System.out.println(
+                            "\n1)Mostrar mapa dos assentos(os lugares ocupados s�o marcados com o n�mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
+                    System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
+                    esc = scanner.nextInt();
+                    break;
+                case 4:
+                    cancelarReserva();
+                    System.out.println(
+                            "\n1)Mostrar mapa dos assentos(os lugares ocupados s�o marcados com o n�mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
+                    System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
+                    esc = scanner.nextInt();
+                    break;
+                case 5:
+                    System.out.println("Obrigado!");
+                    break;
+                default:
+                    System.out.println("Essa n�o � uma op��o dispon�vel");
+                    System.out.println(
+                            "\n1)Mostrar mapa dos assentos(os lugares ocupados s�o marcados com o n�mero 1):\n2)Mostrar quantidade de assentos ocupados e livres:");
+                    System.out.println("3)Reservar um assento:\n4)Cancelar uma reserva de assento: \n5)Sair\n");
+                    esc = scanner.nextInt();
+                    break;
+
+            }
+        }
+
+    }
 }
-
-
-
-
-
-
-
-
